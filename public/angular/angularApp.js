4f33c71482479065644244720197b6ac27d0bafe
Rishabh Agrawal <ragrawal804@gmail.com>
2017-08-26 01:50:10 +0530

    angular chart boilerplate

--- public/angular/angularApp.js
+++ public/angular/angularApp.js
@@ -1,4 +1,4 @@
-angular.module('workApp',['ngMaterial','ui.router'])
+angular.module('workApp',['ngMaterial','ui.router','chart.js'])
 
 .config(function($stateProvider,$urlRouterProvider,$locationProvider){
 
@@ -15,6 +15,33 @@ $locationProvider.html5Mode(true);
        .error(function(response){
            
        })
-    
-};
+    };
+    $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+    $scope.series = ['Series A', 'Series B'];
+    $scope.data = [
+      [65, 59, 80, 81, 56, 55, 40],
+      [28, 48, 40, 19, 86, 27, 90]
+    ];
+    $scope.onClick = function (points, evt) {
+      console.log(points, evt);
+    };
+    $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
+    $scope.options = {
+      scales: {
+        yAxes: [
+          {
+            id: 'y-axis-1',
+            type: 'linear',
+            display: true,
+            position: 'left'
+          },
+          {
+            id: 'y-axis-2',
+            type: 'linear',
+            display: true,
+            position: 'right'
+          }
+        ]
+      }
+    };
 });
