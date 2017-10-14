24ce16bd61ac379f01689a294ab2e930d7657229
Rishabh Agrawal <ragrawal804@gmail.com>
2017-10-15 03:46:12 +0530

    renaming

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 var bodyParser = require('body-parser');
 
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 4000))
 app.use(express.static(__dirname + '/public'))
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 app.use(bodyParser.json({}));
@@ -10,14 +10,14 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/', function(request, response) {
   response.sendFile('index.html');
-})
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
-})
+});
 
 app.post('/add',function(req,res){
     console.log(req.body);
-})
+});
 
 
